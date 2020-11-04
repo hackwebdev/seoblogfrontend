e042735b002f1bc561ba5af7af047dbbccae7bb5
hackwebdev <hackwebdev@gmail.com>
2020-11-04 16:36:11 +0800

    signout and conditionall showing nav links

--- components/Header.js
+++ components/Header.js
@@ -1,53 +1,65 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Link from 'next/link';
-import {APP_NAME} from '../config'
+import Router from 'next/router';
+import { APP_NAME } from '../config';
+import { signout, isAuth } from '../actions/auth';
 import {
   Collapse,
   Navbar,
   NavbarToggler,
-  NavbarBrand,
+  NavbarBrand, 
   Nav,
   NavItem,
   NavLink,
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem,
-  NavbarText
+  DropdownItem
 } from 'reactstrap';
 
-const Header = (props) => {
+const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const toggle = () => setIsOpen(!isOpen);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <div>
       <Navbar color="light" light expand="md">
         <Link href="/">
-            <NavLink className="font-weight-bold">{APP_NAME}</NavLink>
+          <NavLink className="font-weight-bold">{APP_NAME}</NavLink>
         </Link>
         <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar> 
+        <Collapse isOpen={isOpen} navbar>
           <Nav className="ml-auto" navbar>
-            <NavItem>
-                <Link href="/signin">
-                    <NavLink >
-                        Signin 
-                    </NavLink>
-                </Link>
-            </NavItem>
-            <NavItem>
-                <Link href="/signup">
-                    <NavLink >
-                        Signup 
-                    </NavLink>
-                </Link>
-            </NavItem>
+            {!isAuth() && (
+              <>
+                <NavItem>
+                  <Link href="/signin">
+                    <NavLink>Signin</NavLink>
+                  </Link>
+                </NavItem>
+                <NavItem>
+                  <Link href="/signup">
+                    <NavLink>Signup</NavLink>
+                  </Link>
+                </NavItem>
+              </>
+            )}
+
+            {isAuth() && (
+              <NavItem>
+                <NavLink style={{ cursor: 'pointer' }} onClick={() => signout(() => Router.replace(`/signin`))}>
+                  Signout
+                </NavLink>
+              </NavItem>
+            )}
           </Nav>
         </Collapse>
       </Navbar>
     </div>
   );
-}
+};
 
 export default Header;
