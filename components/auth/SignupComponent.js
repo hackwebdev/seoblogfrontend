e32a5341ba0659fe2a5ddf51480c478fb5841b4c
hackwebdev <hackwebdev@gmail.com>
2020-11-04 15:42:13 +0800

    signup action and alerts

--- components/auth/SignupComponent.js
+++ components/auth/SignupComponent.js
@@ -1,10 +1,11 @@
 import { useState } from 'react';
+import { signup } from '../../actions/auth';
 
 const SignupComponent = () => {
     const [values, setValues] = useState({
-        name: 'Basha',
-        email: 'basha@gmail.com',
-        password: 'iloveyou',
+        name: 'Ryan',
+        email: 'ryan@gmail.com',
+        password: 'rrrrrr',
         error: '',
         loading: false,
         message: '',
@@ -15,12 +16,36 @@ const SignupComponent = () => {
 
     const handleSubmit = e => {
         e.preventDefault();
-        console.table({ name, email, password, error, loading, message, showForm });
+        // console.table({ name, email, password, error, loading, message, showForm });
+        setValues({ ...values, loading: true, error: false });
+        const user = { name, email, password };
+
+        signup(user).then(data => {
+            if (data.error) {
+                setValues({ ...values, error: data.error, loading: false });
+            } else {
+                setValues({
+                    ...values,
+                    name: '',
+                    email: '',
+                    password: '',
+                    error: '',
+                    loading: false,
+                    message: data.message,
+                    showForm: false
+                });
+            }
+            console.log(data);
+        });
     };
 
     const handleChange = name => e => {
         setValues({ ...values, error: false, [name]: e.target.value });
     };
+
+    const showLoading = () => (loading ? <div className="alert alert-info">Loading...</div> : '');
+    const showError = () => (error ? <div className="alert alert-danger">{error}</div> : '');
+    const showMessage = () => (message ? <div className="alert alert-info">{message}</div> : '');
 
     const signupForm = () => {
         return (
@@ -62,7 +87,14 @@ const SignupComponent = () => {
         );
     };
 
-    return <>{signupForm()}</>;
+    return (
+        <>
+            {showError()}
+            {showLoading()}
+            {showMessage()}
+            {showForm && signupForm()}
+        </>
+    );
 };
 
 export default SignupComponent;
