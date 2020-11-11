3c47be32d480c464869835257379ad34b2d04d3c
hackwebdev <hackwebdev@gmail.com>
2020-11-11 18:59:35 +0800

    error handling

--- pages/signup.js
+++ pages/signup.js
@@ -6,13 +6,15 @@ import Link from 'next/link';
 const Signup = () => {
     return (
         <Layout>
+        <div className="container-fluid">
             <h2 className="text-center pt-4 pb-4">Signup</h2>
             <div className="row">
                 <div className="col-md-6 offset-md-3">
                     <SignupComponent />
                 </div>
             </div>
-        </Layout>
+        </div>
+    </Layout>
     );
 };
 
